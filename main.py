8b93609a589b11bf8c62976813ce9844f7bbb921
DeqingQu <qu.deqing@gmail.com>
2017-10-07 22:35:58 -0700

    add patch methods

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import webapp2
 
+
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, webapp2!')
+
+
+allowed_methods = webapp2.WSGIApplication.allowed_methods
+new_allowed_methods = allowed_methods.union(('PATCH',))
+webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
