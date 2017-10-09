e4de55b932376c34aaa7cb9d08c158eb67b86d31
DeqingQu <qu.deqing@gmail.com>
2017-10-09 16:10:11 -0700

    revise apis to boats and slips

--- main.py
+++ main.py
@@ -21,18 +21,12 @@ class Slip(ndb.Model):
     departure_history = ndb.JsonProperty()
 
 
-class Fish(ndb.Model):
-    name = ndb.StringProperty(required=True)
-    ph_min = ndb.FloatProperty()
-    ph_max = ndb.FloatProperty()
-
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, MainPage!')
 
 
-class BoatHandler(webapp2.RequestHandler):
+class BoatsHandler(webapp2.RequestHandler):
     def post(self):
         boat_data = json.loads(self.request.body)
         if "name" in boat_data and "type" in boat_data and "length" in boat_data:
@@ -47,6 +41,13 @@ class BoatHandler(webapp2.RequestHandler):
             self.response.write('input parameter error')
             self.response.set_status(422)
 
+    def get(self):
+        query = Boat.query()
+        boats = []
+        for boat in query:
+            boats.append(boat.to_dict())
+        self.response.write(json.dumps(boats))
+
     def get(self, id=None):
         if id:
             key = ndb.Key(urlsafe=id)
@@ -57,7 +58,6 @@ class BoatHandler(webapp2.RequestHandler):
             else:
                 self.response.write('can not find boat')
                 self.response.set_status(404)
-
 
     def delete(self, id=None):
         if id:
@@ -72,16 +72,23 @@ class BoatHandler(webapp2.RequestHandler):
                 self.response.set_status(404)
 
 
-class BoatsHandler(webapp2.RequestHandler):
+# class BoatsHandler(webapp2.RequestHandler):
+#     def get(self):
+#         query = Boat.query()
+#         boats = []
+#         for boat in query:
+#             boats.append(boat.to_dict())
+#         self.response.write(json.dumps(boats))
+
+
+class SplitsHandler(webapp2.RequestHandler):
     def get(self):
-        query = Boat.query()
-        boats = []
-        for boat in query:
-            boats.append(boat.to_dict())
-        self.response.write(json.dumps(boats))
+        query = Slip.query()
+        slips = []
+        for slip in query:
+            slips.append(slip.to_dict())
+        self.response.write(json.dumps(slips))
 
-
-class SlipHandler(webapp2.RequestHandler):
     def post(self):
         slip_data = json.loads(self.request.body)
         if "number" in slip_data:
@@ -107,7 +114,6 @@ class SlipHandler(webapp2.RequestHandler):
                 self.response.write('can not find slip')
                 self.response.set_status(404)
 
-
     def delete(self, id=None):
         if id:
             key = ndb.Key(urlsafe=id)
@@ -120,49 +126,14 @@ class SlipHandler(webapp2.RequestHandler):
                 self.response.set_status(404)
 
 
-class SplitsHandler(webapp2.RequestHandler):
-    def get(self):
-        query = Slip.query()
-        slips = []
-        for slip in query:
-            slips.append(slip.to_dict())
-        self.response.write(json.dumps(slips))
-
-
-class FishHandler(webapp2.RequestHandler):
-    def post(self):
-        fish_data = json.loads(self.request.body)
-        new_fish = Fish(name=fish_data["name"])
-        new_fish.ph_min = fish_data["ph_min"]
-        new_fish.ph_max = fish_data["ph_max"]
-        new_fish.put()
-        fish_dict = new_fish.to_dict()
-        fish_dict["self"] = '/fish/' + new_fish.key.urlsafe()
-        self.response.write(json.dumps(fish_dict))
-
-    def get(self, id=None):
-        if id:
-            f = ndb.Key(urlsafe=id).get()
-            f.ph_min = 100.0
-            f.ph_max = 200.0
-            f.put()
-            f_d = f.to_dict()
-            f_d["self"] = "/fish/" + id
-            self.response.write(json.dumps(f.to_dict()))
-
-
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/fish', FishHandler),
-    ('/fish/(.*)', FishHandler),
-    ('/boat', BoatHandler),
-    ('/boat/(.*)', BoatHandler),
     ('/boats', BoatsHandler),
-    ('/slip', SlipHandler),
-    ('/slip/(.*)', SlipHandler),
+    ('/boats/(.*)', BoatsHandler),
+    ('/slips/(.*)', SplitsHandler),
     ('/slips', SplitsHandler)
 ], debug=True)
